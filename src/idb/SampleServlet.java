4b304786b94eefe03dfcaf32738d37b152ea7e59
yichiliu <yl3057@columbia.edu>
2014-04-02 23:08:52 -0400

    first commit

---
+++ src/idb/SampleServlet.java
@@ -0,0 +1,30 @@
+package idb;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SampleServlet extends HttpServlet {
+   public void doGet(HttpServletRequest request,
+      HttpServletResponse response)
+   throws IOException, ServletException {
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      out.println("<html>");
+      out.println("<head>");
+      String title = "Hello World";
+      out.println("<title>" + title + "</title>");
+      out.println("</head>");
+      out.println("<body bgcolor=black>");
+      out.println("<h1>" + title + "</h1>");
+      String param = request.getParameter("param");
+                
+      if (param != null)
+         out.println("Thanks for the lovely param='" + param + "' binding.");
+
+      out.println("");
+      out.println("");
+   }
+}
