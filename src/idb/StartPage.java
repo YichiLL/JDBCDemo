a00745ce2ea5551dcb8125f03ae3b7dacaae060b
yichiliu <yl3057@columbia.edu>
2014-04-09 00:46:49 -0400

    ADD: cookie working.

--- src/idb/StartPage.java
+++ src/idb/StartPage.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,8 +17,10 @@ import oracle.jdbc.pool.OracleDataSource;
 
 public class StartPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String connect_string = "jdbc:oracle:thin:tx2135/Coms4111@//w4111b.cs.columbia.edu:1521/ADB";
+	// template
 	private Connection conn;
+
+	// template
 
 	public StartPage() {
 		super();
@@ -33,12 +36,28 @@ public class StartPage extends HttpServlet {
 			this.conn = c.getConnection();
 			HtmlTemplate tpl = new HtmlTemplate("OnlineStore - Start Page");
 			Statement stmt = conn.createStatement();
+			response.setContentType("text/html");
 			// template
 
-			ResultSet rset = stmt.executeQuery("select * from users");
-			response.setContentType("text/html"); // template
+			// ResultSet rset = stmt.executeQuery("select * from users");
+			out.println(tpl.getHead());
+			out.println("<h2>Welcome!</h2>");
+			Cookie[] cookies = null;
+			cookies = request.getCookies();
+			if( cookies != null ){
+		         out.println("<h2> Found Cookies Name and Value</h2>");
+		         for (int i = 0; i < cookies.length; i++){
+		            Cookie cookie = cookies[i];
+		            out.print("Name : " + cookie.getName( ) + ",  ");
+		            out.print("Value: " + cookie.getValue( )+" <br/>");
+		         }
+		      }else{
+		          out.println(
+		            "<h2>No cookies founds</h2>");
+		      }
 
 			// template
+			out.println("</center></body></html>");
 		} catch (SQLException e) {
 			out.println(e.getMessage());
 			out.println(1);
@@ -66,30 +85,46 @@ public class StartPage extends HttpServlet {
 			this.conn = c.getConnection();
 			HtmlTemplate tpl = new HtmlTemplate("OnlineStore - Start Page");
 			Statement stmt = conn.createStatement();
+			response.setContentType("text/html");
 			// template
 
 			String uname = request.getParameter("uname");
 			ResultSet rset = stmt
 					.executeQuery("select count(*) as user_exist from users where uname='"
 							+ uname + "'");
-			response.setContentType("text/html");
 			out.println(tpl.getHead());
 			out.println("<h2>Welcome!</h2>");
 
 			if (rset.next()) {
-				int user_exist;
-				user_exist = Integer.parseInt(rset.getString("user_exist"));
+				int user_exist = Integer.parseInt(rset.getString("user_exist"));
 				if (user_exist == 1) {
 					out.println("found user " + uname);
+					rset = stmt
+							.executeQuery("select userid from users where uname='"
+									+ uname + "'");
+					if (rset.next()) {
+						String userid = rset.getString("userid");
+						Cookie c_uname = new Cookie("uname", uname);
+						Cookie c_userid = new Cookie("userid", userid);
+						c_uname.setMaxAge(60 * 60 * 24);
+						c_userid.setMaxAge(60 * 60 * 24);
+						response.addCookie(c_uname);
+						response.addCookie(c_userid);
+						String site = "StartPage";
+						response.setStatus(response.SC_MOVED_TEMPORARILY);
+						response.setHeader("Location", site);
+					} else {
+						out.println("<a href=login.html>Database connection error.</a>");
+					}
+
 				} else {
-					out.println("login failed");
-					// TODO: redirect to login page
+					out.println("<a href=login.html>Username does not exist. (Case sensitive.)<br>"
+							+ "Click to go back.</a>");
 				}
 			}
 
-			out.println("</center></body></html>");
-
 			// template
+			out.println("</center></body></html>");
 		} catch (SQLException e) {
 			out.println(e.getMessage());
 			out.println(1);
